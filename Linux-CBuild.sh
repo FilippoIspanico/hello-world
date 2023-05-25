d014aeb6f9db5c3c78106aa0bbb6d49a70fbb5b4
FilippoIspanico <133004373+FilippoIspanico@users.noreply.github.com>
2023-05-25 18:34:14 +0200

    Update Linux-CBuild.sh

--- Linux-CBuild.sh
+++ Linux-CBuild.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-
+export STARTING_PATH = pwd
 sudo apt install libgtest-dev
 sudo apt install cmake
 sudo apt install g++
 cd /usr/src/gtest && sudo cmake CMakeLists.txt && sudo make
 cd /usr/lib/x86_64-linux-gnu/
 sudo cp libgtest.a libgtest_main.a /usr/lib
-mkdir test
-touch calc.hpp
-touch calc.cpp
-touch calc_test.cpp
+
+cd $STARTING_PATH
 g++ -o calc_test calc_test.cpp calc.cpp -lgtest -lpthread
 ./calc_test
