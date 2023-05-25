138367fea8ae7af4da240d8188e00b5401d30250
FilippoIspanico <133004373+FilippoIspanico@users.noreply.github.com>
2023-05-25 19:22:23 +0200

    Update Linux-SampleTest.sh

--- Linux-SampleTest.sh
+++ Linux-SampleTest.sh
@@ -8,4 +8,4 @@ cd /usr/lib/x86_64-linux-gnu/
 sudo cp libgtest.a libgtest_main.a /usr/lib
 
 cd $STARTING_PATH
-g++ -o sample10_unittest -lgtest -lpthread
+g++ -o sample10_unittest.cc -lgtest -lpthread
