c547607318735e9fe094205a8508b9e2925b3e0f
FilippoIspanico <133004373+FilippoIspanico@users.noreply.github.com>
2023-05-25 19:16:05 +0200

    Create Linux-SampleTest.sh

---
+++ Linux-SampleTest.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+export STARTING_PATH=$(pwd)
+sudo apt install libgtest-dev
+sudo apt install cmake
+sudo apt install g++
+cd /usr/src/gtest && sudo cmake CMakeLists.txt && sudo make
+cd /usr/lib/x86_64-linux-gnu/
+sudo cp libgtest.a libgtest_main.a /usr/lib
+
+cd $STARTING_PATH
+g++ -o sample10_unittest -lgtest -lpthread
