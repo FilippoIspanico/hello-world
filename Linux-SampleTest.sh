843a8e39bfaf1e35657dd8e4f378516a7584faee
FilippoIspanico <133004373+FilippoIspanico@users.noreply.github.com>
2023-05-25 21:53:07 +0200

    Update Linux-SampleTest.sh

--- Linux-SampleTest.sh
+++ Linux-SampleTest.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 export STARTING_PATH=$(pwd)
+
 sudo apt install libgtest-dev
 sudo apt install cmake
 sudo apt install g++
-cd /usr/src/gtest && sudo cmake CMakeLists.txt && sudo make
+
+cd /usr/src/gtest 
+sudo cmake 
+sudo make
 cd /usr/lib/x86_64-linux-gnu/
 sudo cp libgtest.a libgtest_main.a /usr/lib
 
-cd $STARTING_PATH
-g++ -o sample10_unittest.cc -lgtest -lpthread
+./sample1_unittest
