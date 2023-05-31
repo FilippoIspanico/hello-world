363cfb7d4880ae52a1fb479fb0b6637516e942a6
markusnil <60107762+markusnil@users.noreply.github.com>
2023-05-31 17:45:50 +0200

    Update Linux-CBuild.sh

--- Linux-CBuild.sh
+++ Linux-CBuild.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 export STARTING_PATH=$(pwd)
-sudo apt install libgtest-dev
-sudo apt install cmake
-sudo apt install g++
+apt install libgtest-dev
+apt install cmake
+apt install g++
 cd /usr/src/gtest && sudo cmake CMakeLists.txt && sudo make
 cd /usr/lib/x86_64-linux-gnu/
-sudo cp libgtest.a libgtest_main.a /usr/lib
+cp libgtest.a libgtest_main.a /usr/lib
 
 cd $STARTING_PATH
 g++ -o calc_test calc_test.cpp calc.cpp -lgtest -lpthread
